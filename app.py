c92af12e08445324c5038ce1e9e7e53e58595af7
supersheep <supersheepxjy@gmail.com>
2014-04-12 10:52:30 +0800

    serve static

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 #coding=utf-8
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for
 from flask import request
 import dpapi
 import mongocli
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/tourlist/api/<int:tourid>', methods = ['GET'])
 def get_task(tourid):
-    data =  mongocli.find_tour(tourid)
-    if(data):
-    	data['_id'] = None
-    	return jsonify(data)
-    else:
-    	return jsonify({"result":"none"})
+	data =  mongocli.find_tour(tourid)
+	if(data):
+		data['_id'] = None
+		return jsonify(data)
+	else:
+		return jsonify({"result":"none"})
 
 @app.route('/tourlist/api/add', methods = ['POST'])
 def add_task():
@@ -30,7 +30,7 @@ def update_tour(tourid):
 	else:
 		mongocli.update_tour(request.json)
 
-@app.route('/tourlist/api/searchdpshop',methods=['GET'])
+@app.route('/tourlist/api/searchdpshop', methods=['GET'])
 def get_search_dpshop():
 	if not request.args.get('query'):
 		return jsonify({"result":"badrequest"})
@@ -38,5 +38,10 @@ def get_search_dpshop():
 		query = json.loads(request.args.get('query'))
 		return (json.dumps(dpapi.get_nearby_dpinfo(query)))
 
+@app.route('/')
+def index():
+	# return url_for('static', filename='style.css')
+	return  app.send_static_file('html/index.html')
+	# return "aeqwe"
 if __name__ == "__main__":
-    app.run(debug = True)
+	app.run(debug = True)
